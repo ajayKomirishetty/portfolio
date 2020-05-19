18dec3dfb07c440a4ec4782ddd2d232bcc7a927f
Ajay Komirishetty <komirishettyajay@gmail.com>
2020-05-18 22:25:13 -0400

    Updates

---
+++ precache-manifest.bbe96b3f2478e2bb208157c8bebbf1c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d66ab15cb39a785d967c",
+    "url": "/portfolio/static/css/main.6688b4bb.chunk.css"
+  },
+  {
+    "revision": "d66ab15cb39a785d967c",
+    "url": "/portfolio/static/js/main.d66ab15c.chunk.js"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/portfolio/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "341d6ac57f1d660e8a4050d2e8d62095",
+    "url": "/portfolio/index.html"
+  }
+];
